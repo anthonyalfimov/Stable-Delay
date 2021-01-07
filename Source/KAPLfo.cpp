7164731d0da1e7b404b9b109a1e614c841d9ac6a
Anthony Alfimov <Anthony.Alfimov@gmail.com>
2021-01-07 20:35:36 +0300

    Implement LFO modulation module

--- Source/KAPLfo.cpp
+++ Source/KAPLfo.cpp
@@ -8,4 +8,53 @@
   ==============================================================================
 */
 
+#include <JuceHeader.h>
 #include "KAPLfo.h"
+
+KAPLfo::KAPLfo() : mSampleRate (-1.0), mPhase (0.0f)
+{
+    reset();
+}
+
+KAPLfo::~KAPLfo()
+{
+    
+}
+
+void KAPLfo::setSampleRate (double inSampleRate)
+{
+    mSampleRate = inSampleRate;
+}
+
+void KAPLfo::reset()
+{
+    // Reset the phase
+    mPhase = 0.0f;
+    // Clear the buffer
+    zeromem (mBuffer, sizeof (float) * KAP::bufferSize);
+}
+
+void KAPLfo::process(float inRate,
+                     float inDepth,
+                     int inNumSamplesToRender)
+{
+    const float rate = jmap(inRate, 0.0f, 1.0f, 0.01f, 10.0f);
+    
+    for (int i = 0; i < inNumSamplesToRender; ++i)
+    {
+        // Record the LFO state in the buffer
+        const float lfoPosition = inDepth * sinf(mPhase * MathConstants<float>::twoPi);
+        mBuffer[i] = lfoPosition;
+        
+        // Advance the LFO phase
+        mPhase += rate / mSampleRate;
+        
+        if (mPhase > 1.0f)
+            mPhase -= 1.0f;
+    }
+}
+
+const float* KAPLfo::getBuffer() const
+{
+    return mBuffer;
+}
--- Source/KAPLfo.h
+++ Source/KAPLfo.h
@@ -9,3 +9,25 @@
 */
 
 #pragma once
+
+#include "KAPAudioHelpers.h"
+
+class KAPLfo
+{
+public:
+    KAPLfo();
+    ~KAPLfo();
+    
+    void setSampleRate (double inSampleRate);
+    void reset();
+    void process(float inRate,
+                 float inDepth,
+                 int inNumSamplesToRender);
+    
+    const float* getBuffer() const;
+    
+private:
+    double mSampleRate;
+    float mPhase;
+    float mBuffer[KAP::bufferSize];     // TODO: dynamically allocate the buffer
+};
