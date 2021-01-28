1da018b2b49888e4d5f592f6fc36aa1bc093db6c
Anthony Alfimov <Anthony.Alfimov@gmail.com>
2021-01-28 22:20:45 +0300

    Update TODOs

--- Source/KAPGainPanel.cpp
+++ Source/KAPGainPanel.cpp
@@ -34,9 +34,12 @@ void KAPGainPanel::setParameterID (KAPParameter inParameter)
     const int sliderSize = 56;
     
     // TODO: is there a better way to position slider at the center of parent component?
-    //    A: there's `centreWithSize()`, as well as the ability to set size and position separately,
+    //   A1: there's `centreWithSize()`, as well as the ability to set size and position separately,
     //       and set position relative to parent size. But I haven't found another way to set this
     //       up with a single line where you can modify the bounds freely.
+    //   A2: I think that it's much easier to understand the intent of component positioning when
+    //       it's expressed as operations on rectangle objects, rather than math on coordinates and
+    //       dimensions.
     mSlider->setBounds (getLocalBounds().withSizeKeepingCentre (sliderSize, sliderSize).withY (25));
     
     addAndMakeVisible (mSlider.get());
