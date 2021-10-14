9c6cfc2dba4cf6d7c15dbcdfd760b70f7c4b8477
Anthony Alfimov <Anthony.Alfimov@gmail.com>
2021-10-14 17:46:08 +0300

    Fix feedback compensation amount calculation

--- Source/FxModule.cpp
+++ Source/FxModule.cpp
@@ -206,9 +206,8 @@ void FxModule::process (const float* inAudio, float* outAudio,
 
         // Compensate the feedback decay by adding an appropriate amount of
         //  "dry" feedback sample
-        // FIX: This threshold is not the signal position relative to clipping curve!
         const float expectedPeakGain = Decibels::decibelsToGain (preBoostInDb
-                                                                 - thresholdInDb);
+                                                                 - mClippingThreshold);
         const float postClipperGain = SaturationModule::saturateBeta (expectedPeakGain);
         const float attenuationFactor = postClipperGain / expectedPeakGain;
         //DBG(attenuationFactor);
