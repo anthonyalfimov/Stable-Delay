9c007c0099abd8495f89778b5419b2d6135f168e
Anthony Alfimov <Anthony.Alfimov@gmail.com>
2022-11-13 20:21:10 +0400

    Update to JUCE 7.0.2

--- JuceLibraryCode/BinaryData.cpp
+++ JuceLibraryCode/BinaryData.cpp
@@ -4,6 +4,8 @@
 
 */
 
+#include <cstring>
+
 namespace BinaryData
 {
 
@@ -6981,6 +6983,7 @@ static const unsigned char temp_binary_data_2[] =
 const char* OpenSansBold_ttf = (const char*) temp_binary_data_2;
 
 
+const char* getNamedResource (const char* resourceNameUTF8, int& numBytes);
 const char* getNamedResource (const char* resourceNameUTF8, int& numBytes)
 {
     unsigned int hash = 0;
@@ -7015,13 +7018,12 @@ const char* originalFilenames[] =
     "OpenSans-Bold.ttf"
 };
 
+const char* getNamedResourceOriginalFilename (const char* resourceNameUTF8);
 const char* getNamedResourceOriginalFilename (const char* resourceNameUTF8)
 {
     for (unsigned int i = 0; i < (sizeof (namedResourceList) / sizeof (namedResourceList[0])); ++i)
-    {
-        if (namedResourceList[i] == resourceNameUTF8)
+        if (strcmp (namedResourceList[i], resourceNameUTF8) == 0)
             return originalFilenames[i];
-    }
 
     return nullptr;
 }
